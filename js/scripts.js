84fda7ee668a288aefb9e913d750cde82be398f6
Sina <sina@thewolpers.com>
2024-03-06 16:32:49 -0800

    Add all gen 1 starter Pokemon to 'Pokemon List' array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,47 @@
-alert('Hello world');
-
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    { 
+        name: 'bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    { 
+        name: 'ivysaur',
+        height: 1,
+        types: ['grass', 'poison']
+    },
+    { 
+        name: 'venusaur',
+        height: 2,
+        types: ['grass', 'poison']
+    },
+    { 
+        name: 'charmander',
+        height: 0.6,
+        types: ['fire']
+    },
+    { 
+        name: 'charmeleon',
+        height: 1.1,
+        types: ['fire']
+    },
+    { 
+        name: 'charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    { 
+        name: 'squirtle',
+        height: 0.5,
+        types: ['water']
+    },
+    { 
+        name: 'wartortle',
+        height: 1,
+        types: ['water']
+    },
+    { 
+        name: 'blastoise',
+        height: 1.6,
+        types: ['water']
+    }
+];
